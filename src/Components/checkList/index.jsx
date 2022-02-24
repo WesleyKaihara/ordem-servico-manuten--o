0c7f084e4c89d36bdddf9d26f5ce5722380cba3e
Wesley <wesleykaihara@gmail.com>
2022-02-23 21:04:43 -0300

    :construction: CheckList - não  esta c/ BancoDados

---
+++ src/Components/checkList/index.jsx
@@ -0,0 +1,90 @@
+import './style.css';
+
+export default function CheckList() {
+  return (
+    <div className='CheckList'>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="Check2" id="Check2" className='checkbox' />
+        <label for="Ckeck2"> Aparelho Liga</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" id="Check1" className='checkbox' value="" />
+        <label for="Ckeck1"> Touch Funcionando</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1"> Botões de Volume</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1"> Botão Power </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1"> Auto Falante </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Conector de Carga </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Conector Fone de ouvido </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Sinal de rede operadora</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Internet </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Camera Frontal </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Camera traseira </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Flash </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Sensor de presença </label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Bluetooth</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Microfone</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Autofalante Auricular</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Carregando a Bateria</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Carregamento por indução</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">Vibrando</label>
+      </div>
+      <div className='checkList_Item'>
+        <input type="checkbox" name="" className='checkbox' id="Check1" value="" />
+        <label for="Ckeck1">NFC</label>
+      </div>
+
+
+    </div>
+  )
+}
